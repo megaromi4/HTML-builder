2cadda370fa27f8f759be6e271e393a7dd523a08
Your NameRoman <rabcevra@gmail.com>
2022-05-23 23:25:19 +0300

    feat: done 03-files-in-folder

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,11 +1,26 @@
 const fs = require("fs");
+const path = require("path");
+const secretFolder = path.join(__dirname, "secret-folder");
 
-fs.readdir(__dirname, { withFileTypes: true }, (err, files) => {
-  console.log("\nCurrent directory files:");
-  if (err) console.log(err);
-  else {
+fs.readdir(secretFolder, { withFileTypes: true }, (err, files) => {
+  if (err) {
+    console.error(err);
+  } else {
     files.forEach((file) => {
-      console.log(file.dirent.isFile());
+      if (file.isFile()) {
+        let filesSecretFolder = path.join(
+          __dirname,
+          "secret-folder",
+          file.name
+        );
+        fs.stat(filesSecretFolder, (err, stats) => {
+          if (err) console.log("Error: ", err.message);
+          let name = path.parse(file.name).name;
+          let ext = path.parse(file.name).ext.split(".")[1];
+          let size = stats.size;
+          console.log(`${name} - ${ext} - ${size} b`);
+        });
+      }
     });
   }
 });
